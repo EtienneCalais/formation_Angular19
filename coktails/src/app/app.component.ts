71ccbdbedd01453643a1d6f036be1b20a70ff21b
Etienne CALAIS <etienne75012@gmail.com>
2025-04-04 11:03:37 +0200

    initialisation

---
+++ coktails/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello Coktails !</h1>',
+})
+export class AppComponent {}
